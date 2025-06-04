73d235e0ed6254328ebbabf669d0159ab664f873
ITSulik <valmedved8@gmail.com>
2025-06-04 15:03:29 +0300

    Refactor UserController and UserService for improved async handling and code clarity; update user retrieval and registration methods accordingly.

--- src/Users.Api/Controllers/UserController.cs
+++ src/Users.Api/Controllers/UserController.cs
@@ -3,92 +3,63 @@ using Users.Api.Models;
 using Users.Api.Services;
 using Users.Api.Domain.Users;
 using System.Threading.Tasks;
+using System.Linq;
 
-
-namespace Users.Api.Controllers;
-
-[ApiController]
-[Route("users")]
-public class UserController(UserService userService) : ControllerBase
+namespace Users.Api.Controllers
 {
-    //     private readonly UserService _userService = userService;
-
-
-    //     [HttpGet]
-    //     public IActionResult GetAllUsers() =>
-    //     Ok(_userService.GetAllUsers().Select(UserResponse.FromDomainModel));
-
-    //     [HttpGet("{id}")]
-    //     public IActionResult GetUserById(Guid id) =>
-    //     _userService.GetUserById(id) is { } user
-    //         ? Ok(UserDetailResponse.FromDomainModel(user))
-    //         : NotFound();
-
-    //     [HttpPost]
-    //     public IActionResult RegisterUser([FromBody] RegisterUserRequest request)
-    // {
-    //     if (!ModelState.IsValid)
-    //         return BadRequest(ModelState);
-
-    //     var user = _userService.RegisterUser(request);
-    //     return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, UserDetailResponse.FromDomainModel(user));
-    // }
-
-    //     [HttpPut("{id}")]
-    //     public IActionResult UpdateUser(Guid id, [FromBody] UpdateUserRequest request)
-    // {
-    //     if (!ModelState.IsValid)
-    //         return BadRequest(ModelState);
-
-    //     var user = _userService.UpdateUser(id, request);
-    //     return user is not null ? Ok(UserDetailResponse.FromDomainModel(user)) : NotFound();
-    // }
-    //     [HttpDelete("{id}")]
-    //     public IActionResult DeleteUser(Guid id)
-    // {
-    //     var user = _userService.GetUserById(id);
-    //     if (user is null) return NotFound();
-
-    //     _userService.DeleteUser(id);
-    //     return NoContent();
-    // }
-    [HttpGet]
-    public async Task<IActionResult> GetAllUsers()
+    [ApiController]
+    [Route("users")]
+    public class UserController(UserService userService) : ControllerBase
     {
-        var users = await userService.GetAllUsers();
-        return Ok(users.Select(UserResponse.FromDomainModel));
-    }
-    [HttpGet("{id}")]
-    public IActionResult GetUserById(Guid id)
-    {
-        var user = userService.GetUserById(id);
-        return user is not null ? Ok(UserDetailResponse.FromDomainModel(user)) : NotFound();
-    }
-    [HttpPost]
-    public async Task<IActionResult> RegisterUser([FromBody] RegisterUserRequest request)
-    {
-        if (!ModelState.IsValid)
-            return BadRequest(ModelState);
+        private readonly UserService _userService = userService;
 
-        var user = await userService.RegisterUser(request);
-        return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, UserDetailResponse.FromDomainModel(user));
-    }
-    [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateUser(Guid id, [FromBody] UpdateUserRequest request)
-    {
-        if (!ModelState.IsValid)
-            return BadRequest(ModelState);
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _userService.GetAllUsers();
+            return Ok(users.Select(UserResponse.FromDomainModel));
+        }
 
-        var user = await userService.UpdateUser(id, request);
-        return user is not null ? Ok(UserDetailResponse.FromDomainModel(user)) : NotFound();
-    }
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteUser(Guid id)
-    {
-        var user = userService.GetUserById(id);
-        if (user is null) return NotFound();
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(Guid id)
+        {
+            var user = await _userService.GetUserById(id);
+            return user is not null 
+                ? Ok(UserDetailResponse.FromDomainModel(user)) 
+                : NotFound();
+        }
 
-        await userService.DeleteUser(id);
-        return NoContent();
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser([FromBody] RegisterUserRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = await _userService.RegisterUser(request);
+            return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, UserDetailResponse.FromDomainModel(user));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(Guid id, [FromBody] UpdateUserRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = await _userService.UpdateUser(id, request);
+            return user is not null 
+                ? Ok(UserDetailResponse.FromDomainModel(user)) 
+                : NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(Guid id)
+        {
+            var user = await _userService.GetUserById(id);
+            if (user is null) 
+                return NotFound();
+
+            await _userService.DeleteUser(id);
+            return NoContent();
+        }
     }
 }
--- src/Users.Api/Services/UserService.cs
+++ src/Users.Api/Services/UserService.cs
@@ -3,91 +3,67 @@ using Users.Api.Models;
 using Users.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
-using Microsoft.VisualBasic;
 
-namespace Users.Api.Services;
-
-public class UserService(UsersDbContext context)
+namespace Users.Api.Services
 {
-    // private static readonly List<User> _users = new();
-
-    // public IEnumerable<User> GetAllUsers() => _users;
-
-    // public User? GetUserById(Guid id) =>
-    //     _users.FirstOrDefault(u => u.Id == id);
-
-    // public User RegisterUser(RegisterUserRequest request)
-    // {
-    //     var user = new User
-    //     {
-    //         Id = Guid.NewGuid(),
-    //         FirstName = request.FirstName,
-    //         LastName = request.LastName,
-    //         Email = request.Email
-    //     };
-
-    //     _users.Add(user);
-    //     return user;
-    // }
-
-    // public User? UpdateUser(Guid id, UpdateUserRequest request)
-    // {
-    //     var user = _users.FirstOrDefault(u => u.Id == id);
-    //     if (user is null) return null;
-
-    //     user.FirstName = request.FirstName;
-    //     user.LastName = request.LastName;
-    //     user.Email = request.Email;
-
-    //     return user;
-    // }
-
-    // public void DeleteUser(Guid id)
-    // {
-    //     var user = _users.FirstOrDefault(u => u.Id == id);
-    //     if (user is not null)
-    //     {
-    //         _users.Remove(user);
-    //     }
-    // }
-    public readonly UsersDbContext _context = context;
-    public async Task<IEnumerable<User>> GetAllUsers() => await _context.Users.ToListAsync();
-    public User? GetUserById(Guid id) =>
-        _context.Users.FirstOrDefault(u => u.Id == id);
-    public async Task<User> RegisterUser(RegisterUserRequest request)
+    public class UserService
     {
-        var user = new User
+        private readonly UsersDbContext _context;
+
+        public UserService(UsersDbContext context)
         {
-            Id = Guid.NewGuid(),
-            FirstName = request.FirstName,
-            LastName = request.LastName,
-            Email = request.Email
-        };
+            _context = context;
+        }
 
-        _context.Users.Add(user);
-        await _context.SaveChangesAsync();
-        return user;
-    }
-    public async Task<User?> UpdateUser(Guid id, UpdateUserRequest request)
-    {
-        var user = _context.Users.FirstOrDefault(u => u.Id == id);
-        if (user is null) return null;
-
-        user.FirstName = request.FirstName;
-        user.LastName = request.LastName;
-        user.Email = request.Email;
-
-        await _context.SaveChangesAsync();
-        return user;
-    }
-    public async Task DeleteUser(Guid id)
-    {
-        var user = _context.Users.FirstOrDefault(u => u.Id == id);
-        if (user is not null)
+        public async Task<IEnumerable<User>> GetAllUsers()
         {
-            _context.Users.Remove(user);
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User?> GetUserById(Guid id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<User> RegisterUser(RegisterUserRequest request)
+        {
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email
+            };
+
+            _context.Users.Add(user);
             await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        public async Task<User?> UpdateUser(Guid id, UpdateUserRequest request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if (user is null) 
+                return null;
+
+            user.FirstName = request.FirstName;
+            user.LastName = request.LastName;
+            user.Email = request.Email;
+
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        public async Task DeleteUser(Guid id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if (user is not null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
         }
     }
-
 }
